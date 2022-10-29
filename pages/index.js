77fd787a9a9525b838f47ad89e9ab120bd3bd2f3
boxescode <108833715+boxescode@users.noreply.github.com>
2022-10-28 22:25:59 -0400

    exclusive comming soon

--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,14 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+      <Image src="/images/tucks2.jpeg" alt="truck" width={624} height={468} />
         <h2 className={styles.title}>
+
         ESCLUSIVE TRUCKS AND CARS
         </h2>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Comming soon
         </p>
 
         <div className={styles.grid}>
@@ -27,30 +28,11 @@ export default function Home() {
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+          
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+         
         </div>
       </main>
 
